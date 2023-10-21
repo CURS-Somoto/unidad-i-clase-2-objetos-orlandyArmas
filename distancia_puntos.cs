dfc40eb17fb07402837dfb68b470a0f6ff106ed7
orlandyArmas <147268079+orlandyArmas@users.noreply.github.com>
2023-10-21 14:48:47 -0600

    Update distancia_puntos.cs

--- distancia_puntos.cs
+++ distancia_puntos.cs
@@ -1,1 +1,32 @@
+using System;
 
+class Punto
+{
+    public decimal x { get; set; }
+    public decimal y { get; set; }
+
+    public Punto(decimal x, decimal y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double MedirDistancia(Punto puntoDestino)
+    {
+        double distancia = Math.Sqrt(Math.Pow((double)(puntoDestino.x - this.x), 2) + Math.Pow((double)(puntoDestino.y - this.y), 2));
+        return distancia;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Punto puntoOrigen = new Punto(120M, 30M);
+        Punto puntoDestino = new Punto(72M, 80M);
+
+        double distancia = puntoOrigen.MedirDistancia(puntoDestino);
+
+        Console.WriteLine($"La distancia entre dos puntos es: {distancia}");
+    }
+}
